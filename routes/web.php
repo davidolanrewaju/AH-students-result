5981f39e9a78a9a045d16c9f9c0d34bd4daa4da9
David <davidolanrewaju538@gmail.com>
2024-04-05 10:18:17 +0100

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,26 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/student', [StudentController::class, 'index'])->name('student.index');
-Route::post('/student/loggedin', [StudentController::class, 'checkLogin'])->name('student.checkLogin');
-Route::post('/student', [StudentController::class, 'checkSignup'])->name('student.checkSignup');
-Route::get('/student/signup', [StudentController::class, 'signup'])->name('student.signup');
-Route::get('/student/dashboard/{student}', [StudentController::class, 'show'])->name('student.show');
+Route::prefix('student')->group(function() {
+    Route::get('/login', [StudentController::class, 'login'])->name('student.login');
+    Route::get('/dashboard', [StudentController::class, 'show'])->name('student.show');
+    Route::get('/signup', [StudentController::class, 'signup'])->name('student.signup');
+    Route::post('/loggedin', [StudentController::class, 'checkLogin'])->name('student.checkLogin');
+    Route::post('/signedup', [StudentController::class, 'checkSignup'])->name('student.checkSignup');
+});
 
-Route::get('/student/login', [StudentController::class, 'login'])->name('student.login');
+Route::prefix('admin')->group(function () {
+    Route::get('/login', [AdminController::class, 'login'])->name('admin.login');
+    Route::get('/signup', [AdminController::class, 'signup'])->name('admin.signup');
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::post('/loggedin', [AdminController::class, 'checkLogin'])->name('admin.checkLogin');
+    Route::post('/signedup', [AdminController::class, 'checkSignup'])->name('admin.checkSignup');
+});
 
+// Route::get($uri);
 // Route::put($uri, $callback);
 // Route::patch($uri, $callback);
 // Route::delete($uri, $callback);
